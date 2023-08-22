b1a0d96c5874a2a04921ecc62043a64a357b92b9
Gun Lyung You <40779162+gunlyungyou@users.noreply.github.com>
2023-08-22 10:26:19 +0900

    Change src folder

---
+++ CommonLit - Evaluate Student Summaries/src/utils/utils.py
@@ -0,0 +1,184 @@
+# ====================================================
+# Utils
+# ====================================================
+from sklearn.metrics import mean_squared_error
+import os
+import random
+import torch
+import numpy as np
+import torch.nn as nn
+import math
+import time
+from dataset import collate
+
+
+OUTPUT_DIR = './'
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+
+def MCRMSE(y_trues, y_preds):
+    scores = []
+    idxes = y_trues.shape[1]
+    for i in range(idxes):
+        y_true = y_trues[:,i]
+        y_pred = y_preds[:,i]
+        score = mean_squared_error(y_true, y_pred, squared=False) # RMSE
+        scores.append(score)
+    mcrmse_score = np.mean(scores)
+    return mcrmse_score, scores
+
+
+def get_score(y_trues, y_preds):
+    mcrmse_score, scores = MCRMSE(y_trues, y_preds)
+    return mcrmse_score, scores
+
+
+def get_logger(filename=OUTPUT_DIR+'train'):
+    from logging import getLogger, INFO, StreamHandler, FileHandler, Formatter
+    logger = getLogger(__name__)
+    logger.setLevel(INFO)
+    handler1 = StreamHandler()
+    handler1.setFormatter(Formatter("%(message)s"))
+    handler2 = FileHandler(filename=f"{filename}.log")
+    handler2.setFormatter(Formatter("%(message)s"))
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    return logger
+
+LOGGER = get_logger()
+
+
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    
+seed_everything(seed=42)
+
+
+class RMSELoss(nn.Module):
+    def __init__(self, reduction='mean', eps=1e-9):
+        super().__init__()
+        self.mse = nn.MSELoss(reduction='none')
+        self.reduction = reduction
+        self.eps = eps
+
+    def forward(self, y_pred, y_true):
+        loss = torch.sqrt(self.mse(y_pred, y_true) + self.eps)
+        if self.reduction == 'none':
+            loss = loss
+        elif self.reduction == 'sum':
+            loss = loss.sum()
+        elif self.reduction == 'mean':
+            loss = loss.mean()
+        return loss
+    
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (remain %s)' % (asMinutes(s), asMinutes(rs))
+
+
+def train_fn(fold, train_loader, model, criterion, optimizer, epoch, scheduler, device):
+    model.train()
+    scaler = torch.cuda.amp.GradScaler(enabled=CFG.apex)
+    losses = AverageMeter()
+    start = end = time.time()
+    global_step = 0
+    for step, (inputs, labels) in enumerate(train_loader):
+        inputs = collate(inputs)
+        for k, v in inputs.items():
+            inputs[k] = v.to(device)
+        labels = labels.to(device)
+        batch_size = labels.size(0)
+        with torch.cuda.amp.autocast(enabled=CFG.apex):
+            y_preds = model(inputs)
+            loss = criterion(y_preds, labels)
+        if CFG.gradient_accumulation_steps > 1:
+            loss = loss / CFG.gradient_accumulation_steps
+        losses.update(loss.item(), batch_size)
+        scaler.scale(loss).backward()
+        grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), CFG.max_grad_norm)
+        if (step + 1) % CFG.gradient_accumulation_steps == 0:
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad()
+            global_step += 1
+            if CFG.batch_scheduler:
+                scheduler.step()
+        end = time.time()
+        if step % CFG.print_freq == 0 or step == (len(train_loader)-1):
+            print('Epoch: [{0}][{1}/{2}] '
+                  'Elapsed {remain:s} '
+                  'Loss: {loss.val:.4f}({loss.avg:.4f}) '
+                  'Grad: {grad_norm:.4f}  '
+                  'LR: {lr:.8f}  '
+                  .format(epoch+1, step, len(train_loader), 
+                          remain=timeSince(start, float(step+1)/len(train_loader)),
+                          loss=losses,
+                          grad_norm=grad_norm,
+                          lr=scheduler.get_lr()[0]))
+    return losses.avg
+
+
+def valid_fn(valid_loader, model, criterion, device):
+    losses = AverageMeter()
+    model.eval()
+    preds = []
+    start = end = time.time()
+    for step, (inputs, labels) in enumerate(valid_loader):
+        inputs = collate(inputs)
+        for k, v in inputs.items():
+            inputs[k] = v.to(device)
+        labels = labels.to(device)
+        batch_size = labels.size(0)
+        with torch.no_grad():
+            y_preds = model(inputs)
+            loss = criterion(y_preds, labels)
+        if CFG.gradient_accumulation_steps > 1:
+            loss = loss / CFG.gradient_accumulation_steps
+        losses.update(loss.item(), batch_size)
+        preds.append(y_preds.to('cpu').numpy())
+        end = time.time()
+        if step % CFG.print_freq == 0 or step == (len(valid_loader)-1):
+            print('EVAL: [{0}/{1}] '
+                  'Elapsed {remain:s} '
+                  'Loss: {loss.val:.4f}({loss.avg:.4f}) '
+                  .format(step, len(valid_loader),
+                          loss=losses,
+                          remain=timeSince(start, float(step+1)/len(valid_loader))))
+    predictions = np.concatenate(preds)
+    return losses.avg, predictions
+
+
